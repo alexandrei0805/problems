df0bd5a421b0b3a15ca3b22a558d59afc78b3310
alexandrei0805 <116917004+alexandrei0805@users.noreply.github.com>
2022-12-17 11:34:06 +0200

    Add files via upload

---
+++ parcauto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <strcmp.h>
+
+
+typedef struct Masina
+{
+    char* marca;
+    char *model;
+    char tokenMasina[11];
+    char pretAchizitie;
+    char pretVanzare;
+
+}Masina
+
+
+int main(int argc, char *argv[])
+{
+    for(int i<=10 ;)
+
+}
